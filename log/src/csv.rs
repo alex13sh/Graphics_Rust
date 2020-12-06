5288fcfee7cb524a1bba269fdeaa86d38b04e2c6
alex13sh <alex97sh@yandex.ru>
2020-12-06 19:44:43 +0300

    [log, csv] Десериализация в структуру используя chrono::NativeDateTime

--- log/src/csv.rs
+++ log/src/csv.rs
@@ -1,11 +1,26 @@
  use std::fs::File;
  use std::error::Error;
- use serde::Deserialize;
+ use serde::{de, Deserialize, Deserializer};
+ 
+type DateTime = chrono::DateTime<chrono::Local>;
+type DateTimeFix = chrono::DateTime<chrono::FixedOffset>; 
+use chrono::Duration;
+use chrono::NaiveDateTime;
+
+fn naive_date_time_from_str<'de, D>(deserializer: D) -> Result<NaiveDateTime, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let s: String = Deserialize::deserialize(deserializer)?;
+    NaiveDateTime::parse_from_str(&s, "%Y-%m-%dT%H:%M:%S%.f").map_err(de::Error::custom)
+}
 
  #[derive(Debug, Deserialize)]
  pub struct SessionTime {
-    start: String,
-    finish: String,
+    #[serde(deserialize_with = "naive_date_time_from_str")]
+    start: NaiveDateTime,
+    #[serde(deserialize_with = "naive_date_time_from_str")]
+    finish: NaiveDateTime,
  }
  
  pub fn test_read_csv_1(file_path: &str) -> Result<(), Box<dyn Error>> {

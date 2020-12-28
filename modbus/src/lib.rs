08c129c8c652a41f8d204944ddfab5fa13d18800
alex13sh <alex97sh@yandex.ru>
2020-12-28 20:19:26 +0300

    Убрал версии modbus (1, 2, 3). И сотавил одну версию (2)

--- modbus/src/lib.rs
+++ modbus/src/lib.rs
@@ -1,3 +1,35 @@
-#[path = "modbus_2/mod.rs"]
-pub mod modbus;
-pub use modbus::*;
+#![allow(dead_code)]
+
+pub mod init;
+// use init::*;
+
+pub mod value;
+pub mod sensor;
+pub mod device;
+pub mod invertor;
+pub mod owen_digit_io;
+
+pub use value::*;
+pub use init::ValueError;
+pub use sensor::*;
+pub use device::*;
+pub use invertor::*;
+pub use owen_digit_io::DigitIO;
+
+mod modbus_context;
+use modbus_context::ModbusContext;
+
+// #[test]
+pub fn tst() {
+
+//     init::tst();
+    let d = init::init_devices();
+    let devices: Vec<_> = d.into_iter().map(|d| Device::from(d)).collect();
+    dbg!(&devices);
+//     for d in devices {
+//         let r = d.get_ranges_value(8, true);
+//         dbg!(r);
+//     }
+
+}
+

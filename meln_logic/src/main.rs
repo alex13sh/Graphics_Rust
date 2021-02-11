7d6fbcf5fc50264a0b074c9d8e0d77d4a2bc4ba1
Алексей (Работа) <alex76sh1@gmail.com>
2021-02-11 15:40:00 +0300

    [meln_logic] Добавил набор значений датчиков и протестировал работу сообщений

--- meln_logic/src/main.rs
+++ meln_logic/src/main.rs
@@ -1,3 +1,13 @@
+mod structs;
+use structs::invertor_dvij::*;
+
 fn main() {
-    println!("Hello, world!");
+    println!("!!Hello, world!");
+    
+    let dvij = InvertorDvij::new();
+    let _sub = dvij.messages.get_stream()
+        .subscribe(|message| {dbg!(&message);});
+        
+    dvij.set_speed(12_000);
+    dvij.set_hz(300);
 }
--- meln_logic/src/structs.rs
+++ meln_logic/src/structs.rs
@@ -1,9 +1,27 @@
+#![allow(dead_code)]
+
 use epoxy::property::*;
 use epoxy::{binding, Sink};
 
+#[derive(Default)]
 pub struct Dvij {
     pub speed: PropertyRead<u32>,
+    
+    vibra: PropertyRead<f32>,
+    temp_stator: PropertyRead<f32>,
+    temp_rotor: PropertyRead<f32>,
+//     temp_oil_in: PropertyRead<f32>,
+    temp_oil_out:    PropertyRead<f32>,
+    temp_podshipnik: PropertyRead<f32>, // bearing
+    
+    davl_oil_podshipnik_bottom: PropertyRead<f32>,
+    davl_oil_podshipnik_top:    PropertyRead<f32>,
 }
+
+// pub struct Oil {
+//     temp_dvij_out: PropertyRead<f32>,
+//     temp_station_out: PropertyRead<f32>,
+// }
 
 pub struct Vacum {
     pub davl: PropertyRead<f32>,
@@ -38,40 +56,74 @@ impl Invertor {
     }
 }
 
-pub struct InvertorDvij {
-    pub invertor: Invertor,
-    pub dvij: Dvij,
-    pub speed: PropertyRead<u32>,
-    pub vacum: Vacum,
+pub use invertor_dvij::InvertorDvij;
+pub mod invertor_dvij {
+    use super::{Dvij, Vacum, Invertor};
     
-}
-
-impl InvertorDvij {
-    pub fn new() -> Self {
-        let hz = Property::new(0);
-//         let speed = Property::new(0).as_readonly();
-        let speed = binding!{(*hz as u32) * 60};
+    use std::collections::BTreeMap;
+    use epoxy::property::*;
+    use epoxy::{binding, Sink};
+    
+    pub struct InvertorDvij {
+        pub invertor: Invertor,
+        pub dvij: Dvij,
+        pub speed: PropertyRead<u32>,
+        pub vacum: Vacum,
         
-        InvertorDvij {
-            invertor: Invertor {
-                hz: hz,
-            },
-            dvij: Dvij {
+        pub messages: Sink<Message>,
+        subs: Vec<epoxy::Subscription<Message>>,
+    }
+    
+    #[derive(Debug, Clone)]
+    pub enum Message {
+        Start, Stop,
+        SpeedChanged(u32),
+        VacumDowned(f32),
+        VacumUpped,
+    }
+
+    impl InvertorDvij {
+        pub fn new() -> Self {
+            let hz = Property::new(0);
+    //         let speed = Property::new(0).as_readonly();
+            let speed = binding!{(*hz as u32) * 60};
+            let messages = Sink::new();
+            
+            let mut subs = Vec::new();
+            let s = speed.as_stream()
+                .map(|speed| Message::SpeedChanged(*speed))
+                .pipe_into(&messages);
+            subs.push(s);
+                
+            InvertorDvij {
+                invertor: Invertor {
+                    hz: hz,
+                },
+                dvij: Dvij {
+                    speed: speed.clone(),
+                    .. Dvij::default()
+                },
                 speed: speed.clone(),
-            },
-            speed: speed.clone(),
-            vacum: Vacum::new(),
+                vacum: Vacum::new(),
+                
+                messages: messages,
+                subs: subs,
+            }
         }
-    }
-    
-    pub fn stop(&self) {
-        self.invertor.stop();
-    }
-    
-    pub fn set_speed(&self, speed: u32) {
-        self.invertor.hz.set((speed/60) as u16);
-    }
-    pub fn set_hz(&self, hz: u16) {
-        self.invertor.hz.set(hz);
+        
+        pub fn stop(&self) {
+            self.invertor.stop();
+        }
+        
+        pub fn set_speed(&self, speed: u32) {
+            self.invertor.hz.set((speed/60) as u16);
+        }
+        pub fn set_hz(&self, hz: u16) {
+            self.invertor.hz.set(hz);
+        }
+        
+        pub fn get_values(&self) -> BTreeMap<String, f32> {
+            BTreeMap::new()
+        }
     }
 }

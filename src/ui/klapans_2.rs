4732165a03ea808fa23e72e5477c82bcbb06fbce
Алексей (Работа) <alex76sh1@gmail.com>
2021-08-31 19:25:19 +0300

    [complex] Обновил клапана

--- src/ui/klapans_2.rs
+++ src/ui/klapans_2.rs
@@ -8,9 +8,21 @@ use iced::{
 
 use std::collections::BTreeMap;
 
+struct MyKlapan {
+    shk: String,
+    name: String,
+    enb: bool,
+    state: button::State,
+}
+struct MyButton {
+    name: String,
+    enb: bool,
+    state: button::State,
+}
+
 pub struct Klapans {
-    klapans: Vec<(String, bool, button::State)>,
-    buttons: Vec<(String, bool, button::State)>,
+    klapans: Vec<MyKlapan>,
+    buttons: Vec<MyButton>,
     values: modbus::ModbusValues,
 }
 
@@ -23,24 +35,33 @@ pub enum Message {
 impl Klapans {
     pub fn new(values: modbus::ModbusValues) -> Self {
         let klapan_names = [
-            "Двигатель компрессора воздуха",
-            "Клапан нижнего контейнера", // ШК1
-            "Клапан верхнего контейнера", // ШК5
-            "Клапан подачи материала",  // ШК2
-            "Клапан помольной камеры",  // ШК3
-//             "Клапан напуска",           // ШК4
-//             "Клапан насоса М5",         // ШК6
+            ("ШК1", "Клапан нижнего контейнера"), // ШК1
+            ("ШК5", "Клапан верхнего контейнера"), // ШК5
+            ("ШК2", "Клапан подачи материала"),  // ШК2
+            ("ШК3", "Клапан помольной камеры"),  // ШК3
+            ("ШК4", "Клапан напуска"),           // ШК4
+            ("ШК6", "Клапан насоса М5"),         // ШК6
         ];
         let button_names = [
+            "Двигатель компрессора воздуха",
             "Уменьшить давление",
             "Увеличить давление",
-            "Клапаны",
+            "ШК в рабочее положение",
         ];
         Klapans {
             klapans: klapan_names.into_iter()
-                .map(|&n| ((*n).into(), false, Default::default())).collect(),
+                .map(|(shk, n)| MyKlapan {
+                    shk: (*shk).into(),
+                    name: (*n).into(),
+                    enb: false,
+                    state: Default::default()
+                }).collect(),
             buttons: button_names.into_iter()
-                .map(|&n| ((*n).into(), false, Default::default())).collect(),
+                .map(|n| MyButton {
+                    name: (*n).into(),
+                    enb: false,
+                    state: Default::default()
+                }).collect(),
             values: values,
         }
     }
@@ -48,37 +69,42 @@ impl Klapans {
     pub fn update(&mut self, message: Message) {
         match message {
         Message::ToggleKlapan(name, enb) => {
-            self.klapans.iter_mut().find(|s| s.0==name).unwrap().1 = enb;
+            self.klapans.iter_mut().find(|s| s.name==name).unwrap().enb = enb;
             self.values.set_bit(&name, enb).unwrap();
         }
         Message::PressButton(name) => {
-            let mut pb = self.buttons.iter_mut().find(|s| s.0==name).unwrap();
-            match (name.as_str(), pb.1) {
+            let mut pb = self.buttons.iter_mut().find(|s| s.name==name).unwrap();
+            match (name.as_str(), pb.enb) {
             ("Уменьшить давление", false) => {
-                pb.1 = true;
+                pb.enb = true;
                 self.davl_down();
             }, ("Уменьшить давление", true) => {
-                pb.1 = false;
+                pb.enb = false;
 //                 pb.0 = "Увеличить давление".into();
                 self.davl_dis();
 
             }, ("Увеличить давление", false) => {
-                pb.1 = true;
+                pb.enb = true;
 
                 self.davl_up();
             }, ("Увеличить давление", true) => {
-                pb.1 = false;
+                pb.enb = false;
 //                 pb.0 = "Уменьшить давление".into();
 
                 self.davl_dis();
             },
-            ("Клапаны", _) => {
-                pb.1 = !pb.1;
-                let enb = pb.1;
-                self.set_klapan("Клапан нижнего контейнера", enb);
-                self.set_klapan("Клапан верхнего контейнера", enb);
-                self.set_klapan("Клапан подачи материала", enb);
-                self.set_klapan("Клапан помольной камеры", enb);
+            ("ШК в рабочее положение", _) => {
+                pb.enb = !pb.enb;
+                let enb = pb.enb;
+                self.set_klapan("Клапан нижнего контейнера", enb); // ШК1
+                self.set_klapan("Клапан верхнего контейнера", enb); // ШК5
+//                 self.set_klapan("Клапан подачи материала", enb); // ШК2
+                self.set_klapan("Клапан помольной камеры", enb); // ШК3
+            },
+            ("Двигатель компрессора воздуха", _) => {
+                pb.enb = !pb.enb;
+                let enb = pb.enb;
+                self.set_klapan("Двигатель компрессора воздуха", enb);
             },
             _ => {}}
 //             match name {
@@ -95,14 +121,14 @@ impl Klapans {
     pub fn view(&mut self) -> Element<Message> {
         let controls_klapan = self.klapans.iter_mut()
             .fold(Row::new().spacing(5),
-                |row, (ref name, ref check, pb)|
+                |row, MyKlapan {ref name, enb: ref check, state: pb, ..}|
                 row.push(Button::new(pb, Text::new(name))
                 .style(style::Button::Check{checked: *check})
                 .on_press(Message::ToggleKlapan(name.clone(), !check)))
             );
         let controls_buttons = self.buttons.iter_mut()
             .fold(Row::new().spacing(5),
-                |row, (ref name, ref check, pb)|
+                |row, MyButton{ref name, enb: ref check, state: pb}|
                 row.push(Button::new(pb, Text::new(name))
                 .style(style::Button::Check{checked: *check})
                 .on_press(Message::PressButton(name.clone())))
@@ -116,8 +142,8 @@ impl Klapans {
 
 impl Klapans {
     fn set_klapan(&mut self, name: &str, enb: bool) {
-        if let Some(v) = self.klapans.iter_mut().find(|s| s.0==name) {
-            v.1 = enb;
+        if let Some(v) = self.klapans.iter_mut().find(|s| s.name==name) {
+            v.enb = enb;
         }
         if let Err(e) = self.values.set_bit(name, enb) {
             dbg!(e);
@@ -125,8 +151,8 @@ impl Klapans {
     }
 
     fn set_button(&mut self, name: &str, enb: bool) {
-        if let Some(v) = self.buttons.iter_mut().find(|s| s.0==name) {
-            v.1 = enb;
+        if let Some(v) = self.buttons.iter_mut().find(|s| s.name==name) {
+            v.enb = enb;
         }
     }
 

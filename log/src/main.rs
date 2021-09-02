5b78c915533288612f3f5ac28c763182df460327
Алексей (Работа) <alex76sh1@gmail.com>
2021-09-02 19:37:17 +0300

    [log] Подсчёт времени работы на разных частотах.

--- log/src/main.rs
+++ log/src/main.rs
@@ -5,7 +5,7 @@ use std::time::Duration;
 
 type MyResult = Result<(), Box<dyn std::error::Error>>;
 
-fn main() -> MyResult {
+fn main_1() -> MyResult {
 //     convert_json_old_new()?;
 //     convert_json2csv()?;
 //     test_read_csv_2()?;
@@ -29,6 +29,10 @@ fn main() -> MyResult {
 //     find_1("value_18_03_2021__13_18_44_814534674");
     
     Ok(())
+}
+
+fn main() -> MyResult {
+    calc_hz()
 }
 
 fn filter_values(file_name: &str) -> crate::MyResult {
@@ -55,63 +59,29 @@ fn filter_values(file_name: &str) -> crate::MyResult {
     Ok(())
 }
 
-fn filter_values_2(file_name: &str) -> crate::MyResult {
+// Подсчёт времени работы на частотах.
+fn calc_hz() -> crate::MyResult {
     let hashs = vec![
         ("Скорость", "4bd5c4e0a9"),
-        ("Ток", "5146ba6795"),
-        ("Вибродатчик", "OwenAnalog/3/value"),
-        ("Температура ротора", "OwenAnalog/1/value"),
-        ("Температура статора", "OwenAnalog/4/value"),
-        ("Температура масла на выходе дв. М1 Низ", "OwenAnalog/6/value"),
-        ("Температура подшипника дв. М1 верх", "OwenAnalog/5/value"),
     ];
-    structs::Converter::new(crate::get_file_path("tables/csv/"), crate::get_file_path("tables/excel/"))
-        .read_file_opt(file_name, csv::read_values).ok_or("Ошибка чтения файла")?
-        .fields(hashs)
-        .make_values_1(Duration::from_millis(100))?
-        .write_excel()?;
-    Ok(())
-}
-
-// За 11 секунд и 30-40 мб озу
-fn test_speed() -> MyResult {
-    let paths = vec![
-        "values_27_08_2020__13_08_30_042.json",
-        "values_07_09_2020__13_02_37_096.json",
-        "values_25_08_2020__13_41_06_111.json",
-        "values_26_08_2020__16_26_04_840.json",
-        "values_07_09_2020__16_13_35_221.json",
-        "values_28_08_2020__16_57_26_959.json",
-        "values_08_09_2020__14_28_27_576.json",
-        "values_08_09_2020__14_28_33_906.json",
-        "values_10_09_2020__15_36_13_274.json",
-        "values_28_08_2020__17_06_20_523.json",
-        "values_21_08_2020__17_31_00_188.json",
-        "values_26_08_2020__15_48_12_214.json",
-        "values_26_08_2020__16_05_51_804.json",
-        "values_25_08_2020__15_15_21_933.json",
-        "values_24_08_2020__19_19_10_684.json",
-        "values_24_08_2020__19_03_16_045.json",
-        "values_24_08_2020__18_31_00_766.json",
-    ];
-    for _ in 1..10 {
-        for path in &paths {
-            json::convert::convert_log_file(path, "Log/", "test_log")?;
+    let mut all_time_work_hz = vec![0_f32; 24];
+    for f in get_file_list("tables/csv/")/*.iter().take(2)*/ {
+        let values_log = csv::read_values(&f).ok_or("Ошибка чтения файла")?;
+        let stat = structs::InputValues::from_log_values(values_log)
+            .fields(hashs.clone())
+            .make_values_3(Duration::from_millis(100))
+                .fill_empty()
+                .insert_time_f32()
+            .get_state_build()
+                .time_work_hz()
+                .fields;
+        let time_work_hz = stat.time_work_hz.clone().unwrap();
+        for (t, hz) in time_work_hz.into_iter().zip(0..) {
+            all_time_work_hz[hz] += t;
         }
     }
+    dbg!(all_time_work_hz);
     Ok(())
-}
-
-fn json_get_all_hash() -> MyResult {
-    let js = json::open_json_file("values_14_09_2020__13_24_19_668.json");
-    let hashs = js.get_all_hash();
-    dbg!(hashs.len(), hashs);
-    Ok(())
-}
-
-fn read_csv() {
-    csv::test_read_csv_1("./log/sessions_1.csv")
-        .unwrap();
 }
 
 fn convert_json_old_new() -> MyResult {
@@ -146,42 +116,17 @@ fn convert_json2csv() -> MyResult {
     Ok(())
 }
 
-// <<<<<<< Updated upstream
 
-// fn convert_session() -> MyResult {
-//     let session_path_1 = get_file_path("log/sessions_1.csv");
-//     let session_path_2 = get_file_path("csv/sessions.csv");
-//     
-//     let sessions = csv::read_session(&session_path_1).ok_or("")?;
-//     let sessions: Vec<_> = sessions.into_iter()
-//         .map(|mut s| {
-//             s.set_file_name(format!("value_{}.csv", date_time_to_string_name(&start)));
-//             s
-//         })
-//         .collect();
-//     csv::write_session(&session_path_2, sessions)?;
-//     Ok(())
-// }
-
-fn get_file_list(dir: impl Into<PathBuf>) -> Vec<PathBuf> {
-    let dir = dir.into();
-    let dir_str = dir.to_str().unwrap();
-    let path = get_file_path(dir_str);
+fn get_file_list(dir: &str) -> Vec<PathBuf> {
+    let path = get_file_path(dir);
     let paths = std::fs::read_dir(path).unwrap();
 //     dbg!(paths);
     paths.filter_map(|res| res.ok())
     .map(|dir| dir.path())
     .filter(|path| 
         if let Some(ext) = path.extension() {
-            ext == "json"
+            ext == "csv"
         } else {false}
     ).collect()
 }
 
-fn test_read_csv_2() -> MyResult {
-    let tmp_path = get_file_path("csv/");
-    
-    let values = csv::read_values(&tmp_path.join("values_07_09_2020__16_42_09_399.csv")).ok_or("")?;
-    dbg!(values);
-    Ok(())
-}

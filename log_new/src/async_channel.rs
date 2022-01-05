5cb420df90f94914c98c438f3cf7bd662198e31c
alex13sh <alex97sh@yandex.ru>
2022-01-05 17:12:26 +0300

    [log, async, stat] Доделал статистику

--- log_new/src/async_channel.rs
+++ log_new/src/async_channel.rs
@@ -2,6 +2,7 @@
 
 pub struct Sender<T>(async_broadcast::Sender<T>);
 use futures::{Stream, StreamExt};
+use std::collections::HashMap;
 
 impl <T: Clone> Sender<T> {
     pub async fn send(&self, m: T) {
@@ -25,7 +26,7 @@ pub fn broadcast<T>(cap: usize) -> (Sender<T>, async_broadcast::Receiver<T>)
 #[derive(Clone, Default)]
 pub struct LogState {
     pub cnt: usize,
-    date_time: Option<crate::utils::DateTimeFix>,
+    pub date_time: Option<crate::utils::DateTimeFix>,
     pub time_all: f32,
     pub time_acel: f32,
     pub time_work: f32,
@@ -34,6 +35,8 @@ pub struct LogState {
     pub vibro_max: f32,
     pub hz_vibro: u32, // Зона вибрации
     pub tok_max: u32,
+    
+    pub temps: HashMap<String, (f32, f32)>,
 }
 
 impl LogState {
@@ -41,6 +44,7 @@ impl LogState {
         self.cnt += 1;
         
         if let Some(prev_line) = prev_line {
+            self.time_all = (self.date_time.unwrap().timestamp_millis() - line.date_time.timestamp_millis()) as f32 / 1000.0;
             for (p, v) in prev_line.iter_values_date().zip(line.iter_values_date()) {
                 self.apply_value(v.clone());
                 self.apply_dlt_value(p, v);
@@ -57,18 +61,28 @@ impl LogState {
         let dt = value.date_time;
         let sdt = self.date_time.as_ref().unwrap();
         match value.value.get_sensor_value() {
-        ("Вибродатчик", value) => self.vibro_max = self.vibro_max.max(value),
+        ("Вибродатчик", value) => if self.vibro_max < value {
+            self.vibro_max = value;
+            self.hz_vibro = self.hz_max; // При разгоне, текущая скорость = максимальная скорость
+        }
         ("Скорость", value) => if self.hz_max <= value as u32 {
             self.hz_max = value as u32;
             self.time_acel = (dt.timestamp_millis() - sdt.timestamp_millis()) as f32 / 1000.0;
+            // self.time_acel = self.time_all;
         }
         ("Ток", value) => self.tok_max = self.tok_max.max(value as u32),
-        
+        (sensor, value) if sensor.starts_with("Температура") => {
+            if let Some(ref mut temp) = self.temps.get_mut(sensor) {
+                temp.1 = value;
+            } else {
+                self.temps.insert(sensor.into(), (value, value));
+            }   
+        }
         _ => {}
         }
     }
     
-    fn apply_dlt_value(&mut self, prev_value: crate::value::LogValueHum, value: crate::value::LogValueHum) {
+    fn apply_dlt_value(&mut self, _prev_value: crate::value::LogValueHum, _value: crate::value::LogValueHum) {
         
     }
 }

7c4522be58157a2574b97584adb18e7e5dd28c1a
Алексей (Работа) <alex76sh1@gmail.com>
2022-05-06 14:20:30 +0300

    [log, state] Промежуточный результат. Формирую структуры.

--- log_new/src/stat_info/advance.rs
+++ log_new/src/stat_info/advance.rs
@@ -8,15 +8,55 @@
 /// 1) Масса материала - входной параметр.
 /// 2) Удельная энергия на грамм и килограмм.
 
-use crate::utils::DateTimeFix as DateTime;
+use crate::utils::{
+    DateTimeFix as DateTime,
+    date_time_now
+};
 
 type TimeInterval = f32;
 
 struct StateInfo {
     material_time: TimeInterval,
+    max_values: MaxValues,
+    energy: Energy,
+    material: Option<StateMaterial>,
+}
 
+#[derive(Default)]
+struct MaxValues {
+    power: f32, /// мощность
+    amper: f32, /// ток
+    vibro: f32, /// вибрация
+}
+
+#[derive(Default)]
+struct Energy {
+    sum_watt: f32,
+    time: TimeInterval,
+}
+
+struct WattBeforeMaterial {
+    watt: f32,
 }
 
 struct StateMaterial {
-//     time:
+    start_time: DateTime,
+    finish_time: DateTime,
+    energy: Energy,
+    watt_before: WattBeforeMaterial,
+}
+
+impl StateMaterial {
+    fn start() -> Self {
+        let cur_time = date_time_now();
+        StateMaterial {
+            start_time: cur_time.clone(),
+            finish_time: cur_time,
+
+        }
+    }
+
+    fn finish(self) {
+
+    }
 }
